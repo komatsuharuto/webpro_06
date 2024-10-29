c477b54108dbab91c0b965cefe9e10ce32ce3a87
komatsuharuto <s24G1059ZN@s.chibakoudai.jp>
2024-10-29 11:48:18 +0900

    勝敗の判定の追加

--- app5.js
+++ app5.js
@@ -32,15 +32,26 @@ app.get("/janken", (req, res) => {
   let win = Number( req.query.win );
   let total = Number( req.query.total );
   console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  const num = Math.floor( Math.random() * 3  + 1);
   let cpu = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
+  let judgement = '';
+  if (hand == cpu) {
+    judgement = 'あいこ'; // 引き分け
+  } else if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1;
+  } else {
+    judgement = '負け';
+  }
+
   total += 1;
   const display = {
     your: hand,
